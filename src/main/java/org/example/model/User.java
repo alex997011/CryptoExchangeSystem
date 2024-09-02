2e124dc3fcfd774b4b74061eac016b9b14d95ebe
Alejandro <alejandropacheog45@gmail.com>
2024-09-01 23:48:25 -0500

    implementation of the User, Wallet and Order classes

--- src/main/java/org/example/model/User.java
+++ src/main/java/org/example/model/User.java
@@ -1,4 +1,68 @@
 package org.example.model;
 
 public class User {
+    private int id;
+    private String User;
+    private String password;
+    private String name;
+    private String lastname;
+
+    public User(int id, String user, String password, String name, String lastname) {
+        this.id = id;
+        User = user;
+        this.password = password;
+        this.name = name;
+        this.lastname = lastname;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUser() {
+        return User;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setUser(String user) {
+        User = user;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", User='" + User + '\'' +
+                ", password='" + password + '\'' +
+                ", name='" + name + '\'' +
+                ", lastname='" + lastname + '\'' +
+                '}';
+    }
 }
